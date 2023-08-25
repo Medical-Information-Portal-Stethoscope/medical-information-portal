69f09a2ba1a73f077cc028060b874b6f410af127
Gritsenko Serge <105641816+GritsenkoSerge@users.noreply.github.com>
2023-08-25 15:10:57 +0300

    Update init-letsencrypt.sh

--- infra/prod/init-letsencrypt.sh
+++ infra/prod/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker compose)" ]; then
   exit 1
 fi
 
-domains=(stethoscope.acceleratorpracticum.ru)
+domains=(stethoscope-portal.ru)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="gritsenko.serge.2013@yandex.ru"
